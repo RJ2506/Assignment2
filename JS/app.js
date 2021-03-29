b281763ce5d4efaa1c0b9fae7898d78133e3ae41
RJ2506 <77298403+RJ2506@users.noreply.github.com>
2021-03-29 12:40:18 -0700

    created a comments for JS for part 2

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+/* add a function for the dark button with a
+    toggle(click) on and off
+
+    create a function that will allow the button to create a note taking area
+    with a save and cancel button
+
+    create a funtion that stores the notes in notesArray
+    should contain note objects with body and title
+
+*/
