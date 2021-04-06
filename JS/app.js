bdbf1fbb8b3ed82c8168afbf80842f19ea209572
RJ2506 <77298403+RJ2506@users.noreply.github.com>
2021-04-05 20:58:13 -0700

    created a note button

--- JS/app.js
+++ JS/app.js
@@ -5,16 +5,57 @@ const footer = document.querySelector('.footer')
 const buttons = document.querySelector('.buttons')
 const switch_bar = document.querySelector('.dark')
 const side_button = document.querySelector('.side_button')
-const log = console.log;
+const textinput = document.querySelector('.textinput')
+const textarea = document.querySelector('.textarea')
+const save = document.querySelector('.save')
+const deleted = document.querySelector('.delete')
+const button = document.createElement( 'button')
 
-function theme() {
-    head.classList.toggle('toggle-dark');
-    sidebar.classList.toggle('toggle-dark');
-    content.classList.toggle('toggle-dark');
-    footer.classList.toggle('toggle-dark');
-    side_button.classList.toggle('toggle-dark');
-    switch_bar.classList.toggle('toggle_dark_button')
+textinput.classList.remove('textinput')
+textarea.classList.add('textarea')
 
+function note() {
+    textinput.classList.toggle('textinput')
+    const add = document.getElementById("add");
+var i = 0;
+const div = document.getElementById("hobby");
+add.addEventListener("click", function(){
+  i++;
+  const edit = document.createElement('input');
+  edit.id = i;
+  edit.placeholder = "More hobbies";
+  
+  //Also I need add remove button for each input, which removes its input
+  div.appendChild(edit);
+  
+  var button = document. createElement("button");
+  button. innerHTML = "X";
+  button.id=i;
+  button.onclick = function() { 
+         div.removeChild(document.getElementById(button.id));
+         div.removeChild(button)
+ }
+  div.appendChild(button);
+  
+ 
+});
 }
 
+function theme() {
+    head.classList.toggle('toggle-dark')
+    sidebar.classList.toggle('toggle-dark')
+    content.classList.toggle('toggle-dark')
+    footer.classList.toggle('toggle-dark')
+    side_button.classList.toggle('toggle-dark')
+    switch_bar.classList.toggle('toggle_dark_button')
+    textinput.classList.toggle('textinput')
+    textinput.classList.toggle('toggle_textinput')
+    save.classList.toggle('save')
+    save.classList.toggle('toggle-save')
+    deleted.classList.toggle('delete')
+    deleted.classList.toggle('toggle-delete')
+}
+
+
 switch_bar.addEventListener('click', theme, false)
+textarea.addEventListener('click', note, false)
